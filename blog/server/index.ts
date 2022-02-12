29b958e67ad9c20367ae962dd8f9b56d0597084e
hushin <ngtv.hsn@gmail.com>
2022-02-12 17:05:37 +0900

    fastifyNext 導入

--- blog/server/index.ts
+++ blog/server/index.ts
@@ -1,48 +1,37 @@
 import Fastify from 'fastify'
 import fastifyHttpProxy from 'fastify-http-proxy'
-import next from 'next'
+import fastifyNext from 'fastify-nextjs'
 
-const server = Fastify({ logger: true })
+const server = Fastify({ logger: { level: 'error' } })
 
 const port = parseInt(process.env.PORT || '3000', 10)
 const env = process.env.NODE_ENV
 const dev = env !== 'production'
-const app = next({
-  dir: '.', // base directory where everything is, could move to src later
-  dev,
-})
-const handle = app.getRequestHandler()
 
-server.register(fastifyHttpProxy, {
-  upstream: 'http://localhost:1337',
-  prefix: '/graphql',
-  rewritePrefix: '/graphql',
-})
-
-app.prepare().then(() => {
-  if (dev) {
-    server.get('/_next/*', (req, reply) => {
-      return handle(req.raw, reply.raw).then(() => {
-        reply.sent = true
+const start = async () => {
+  try {
+    await server
+      .register(fastifyNext, {
+        dir: '.', // base directory where everything is, could move to src later
+        dev,
       })
+      .after(() => {
+        server.next('*')
+      })
+    await server.register(fastifyHttpProxy, {
+      upstream: 'http://localhost:1337',
+      prefix: '/graphql',
+      rewritePrefix: '/graphql',
     })
-  }
-  server.all('/*', (req, reply) => {
-    return handle(req.raw, reply.raw).then(() => {
-      reply.sent = true
-    })
-  })
-  server.setNotFoundHandler((request, reply) => {
-    return app.render404(request.raw, reply.raw).then(() => {
-      reply.sent = true
-    })
-  })
-  server.listen(port, (err) => {
-    if (err) throw err
-    server.log.info(
+    await server.listen(port)
+    console.log(
       `> Server listening at http://localhost:${port} as ${
         dev ? 'development' : process.env.NODE_ENV
       }`
     )
-  })
-})
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+}
+start()
