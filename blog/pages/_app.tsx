79a6b0b17ee1cdf9c4cfa59ba247008e7e4fe723
hushin <ngtv.hsn@gmail.com>
2022-02-13 11:28:14 +0900

    Suspense が動くように修正

--- blog/pages/_app.tsx
+++ blog/pages/_app.tsx
@@ -2,14 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { RelayEnvironmentProvider } from 'react-relay'
 import { relayEnvironment } from '../src/lib/relayEnvironment'
-import React from 'react'
+import { Suspense } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RelayEnvironmentProvider environment={relayEnvironment}>
-      <React.Suspense fallback="Loading...">
+      <Suspense fallback="Loading...">
         <Component {...pageProps} />
-      </React.Suspense>
+      </Suspense>
     </RelayEnvironmentProvider>
   )
 }
